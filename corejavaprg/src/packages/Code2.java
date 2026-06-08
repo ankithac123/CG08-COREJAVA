8992a4e6e43533338daad30150553d269dad7ad9
likit <likit@LIKITHA886101>
2026-06-08 18:48:34 +0530

    Commit

---
+++ corejavaprg/src/packages/Code2.java
@@ -0,0 +1,17 @@
+package packages;
+
+public class Code2 {
+		public int a=9;
+		private String b="kusuma";
+		public void display() {
+			System.out.println("welcome");
+	    }
+		public static void main(String[]args) {
+			Code2 c=new Code2();
+			
+			System.out.println(c.a);
+			System.out.println(c.b);
+		}
+		
+	}
+
