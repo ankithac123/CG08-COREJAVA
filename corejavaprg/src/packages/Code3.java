ba29a20464cc7a8bf5a7814127488da4532deb39
likit <likit@LIKITHA886101>
2026-06-08 18:57:41 +0530

    Commit

---
+++ corejavaprg/src/packages/Code3.java
@@ -0,0 +1,25 @@
+package packages;
+
+public class Code3 {
+		public int a=9;
+		private String b="kusuma";
+		char w='i';
+		protected int s=7;
+		
+		public void display() {
+			System.out.println("welcome");
+	    }
+		public static void main(String[]args) {
+			Code3 c=new Code3();
+			
+			System.out.println(c.a);
+			System.out.println(c.b);
+			System.out.println(c.s);
+			System.out.println(c.w);
+			
+		}
+		
+		
+	}
+
+
