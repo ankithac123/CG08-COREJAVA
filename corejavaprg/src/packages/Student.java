1f81cfaa73b6ebdc7a6679de569d84243bf63b8f
likit <likit@LIKITHA886101>
2026-06-10 05:26:57 +0530

    commit

---
+++ corejavaprg/src/packages/Student.java
@@ -0,0 +1,25 @@
+package packages;
+
+public class Student {
+	int id;
+	String name;
+
+	Student(int id,String name ){
+	        this.id=id;
+	        this.name=name;
+	}
+
+	void display() {
+	        System.out.println("ID :"+id);
+	        System.out.println("Name :"+name);
+	}
+	public static void main(String[] args) {
+	        Student s=new Student(101,"surya");
+	        Student s1=new Student(103,"yurya");
+	        //boolean output=s1.(instanceof)Student();
+	        s.display();
+	        s1.display();
+	}
+	}
+
+
