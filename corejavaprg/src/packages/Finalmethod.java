582c9c62502c0815bc283b534085c0a1f5ba9752
likit <likit@LIKITHA886101>
2026-06-26 17:38:50 +0530

    commit

---
+++ corejavaprg/src/packages/Finalmethod.java
@@ -0,0 +1,18 @@
+package packages;
+
+class father {
+     final void display() {
+             System.out.println("welcome to java");
+     }
+}
+
+public class Finalmethod extends father {
+    
+     public static void main()
+     {
+    	Finalmethod obj =new
+    Finalmethod();
+    	obj.display();
+     }
+
+}
