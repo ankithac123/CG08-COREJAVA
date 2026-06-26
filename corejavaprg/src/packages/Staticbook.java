e3ffd75daddf6b86be565f73b91820e98e960080
likit <likit@LIKITHA886101>
2026-06-26 17:13:18 +0530

    commit

--- corejavaprg/src/packages/Staticbook.java
+++ corejavaprg/src/packages/Staticbook.java
@@ -6,7 +6,7 @@ package packages;
 	                System.out.println("welcome to java");
 	        }
 	        public static void main(String[] args) {
-	                System.out.println("welcome to python");
+	                System.out.println("welcome to helloword");
 	        }
 	}
  
