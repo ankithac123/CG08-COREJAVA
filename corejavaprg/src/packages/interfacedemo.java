0bfcb0051b218d07dfbb6d12774b92f9564eb5fd
likit <likit@LIKITHA886101>
2026-06-26 17:41:47 +0530

    commit

---
+++ corejavaprg/src/packages/interfacedemo.java
@@ -0,0 +1,41 @@
+package packages;
+
+//demo for normal interface example
+
+interface SmartDevice{
+     void TurnOn();
+     void TurnOff();
+     void Getstatus();
+}
+//class implementing the interface
+
+class Smartlight implements SmartDevice{
+
+     @Override
+     public void TurnOn() {
+     System.out.println("smart light is on");        
+     }
+
+     @Override
+     public void TurnOff() {
+     System.out.println("smart light is off");        
+             
+     }
+
+     @Override
+     public void Getstatus() {
+             
+             System.out.println("smart light is standly mode");
+     }
+     
+}
+
+public class interfacedemo {
+public static void main(String[] args) {
+     Smartlight s=new Smartlight();
+     s.TurnOff();
+     s.TurnOn();
+     s.Getstatus();
+}
+}
+
