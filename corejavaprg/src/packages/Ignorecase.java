e872cfa544804ba8af32aecdc2c162b8da1d669b
likit <likit@LIKITHA886101>
2026-06-27 19:18:54 +0530

    commit

---
+++ corejavaprg/src/packages/Ignorecase.java
@@ -0,0 +1,13 @@
+package packages;
+
+public class Ignorecase {
+//demo for equal ignore case
+
+       public static void main(String[] args) {
+               String s1="sushma";
+               String s2="SUSHMA";
+               System.out.println(s1.equalsIgnoreCase(s2));
+               System.out.println(s1.equals(s2));
+       }
+
+}
