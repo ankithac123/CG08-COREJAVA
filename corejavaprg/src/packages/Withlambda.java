954b716d541020cb47687d3586b6a9c2cb7e99d0
likit <likit@LIKITHA886101>
2026-06-30 15:01:24 +0530

    commit

---
+++ corejavaprg/src/packages/Withlambda.java
@@ -0,0 +1,16 @@
+package packages;
+//demo for lambda expression
+	@FunctionalInterface
+	interface Drawable{
+	        public void draw();
+	}
+
+	public class Withlambda {
+	public static void main(String[] args) {
+	        int width=10;
+	        //with lambda        
+	        Drawable d=()->{System.out.println("Drawing "+width);};
+	        d.draw();
+	}
+	}
+	
