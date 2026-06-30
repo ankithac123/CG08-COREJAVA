8c882307f3abd4133fc22050a13fa07c65e8292a
likit <likit@LIKITHA886101>
2026-06-30 14:08:32 +0530

    commit

---
+++ corejavaprg/src/packages/ArrayListdemo.java
@@ -0,0 +1,25 @@
+package packages;
+import java.util.ArrayList;
+
+// demo for arraylist
+
+public class ArrayListdemo {
+        public static void main(String[] args) {
+                ArrayList al=new ArrayList();
+                
+                al.add(2);
+                al.add("banana");
+                al.add(9.0);
+                al.add('d');
+                
+                System.out.println(al);
+                
+        //        al.clear();
+                System.out.println(al);
+                al.getFirst();
+                System.out.println(al);
+                al.set(3, "cherry");
+                System.out.println(al);
+        }
+
+}
